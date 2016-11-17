b49a331b873fd3db3a3d78ed6c04cfd6d0dd9207
森篤史 <e1237@s.akashi.ac.jp>
2016-11-17 10:18:11 +0900

    デバッグ文の削除、コメントの追加

--- kadai1/1.py
+++ kadai1/1.py
@@ -7,11 +7,10 @@ import sys
 
 if __name__ == '__main__':
 	args = sys.argv #コマンドライン引数を取得
-	print args
 	if len(args) != 3: #コマンドライン引数が3つでなければ	
 		print '引数が間違っています'
 	else:
-		img = cv2.imread(args[1], cv2.IMREAD_UNCHANGED)
+		img = cv2.imread(args[1], cv2.IMREAD_UNCHANGED) #読み込み
 		cv2.imshow(args[1], img) #表示
 		cv2.waitKey(0)
 		cv2.destroyAllWindows()
