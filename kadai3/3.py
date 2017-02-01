aebfe19dc39cc16b87d9d902c5b6378259b4d817
Mori-Atsushi <e1237@s.akashi.ac.jp>
2017-02-01 23:17:32 +0900

    問3 完了

---
+++ kadai3/3.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+# ファイル名を指定して画像をDCT変換、さらに逆DCT変換を行い、元の画像とのSN比を求める。
+
+import numpy as np
+import cv2
+import sys
+import dct
+import math
+
+def filter(data, per):
+    arr = np.array([])
+    for d in data:
+        arr = np.append(arr, np.array(d), axis=0)
+    arr = map(lambda x: math.fabs(x), arr)
+    arr = np.sort(arr)
+    threshold = arr[int(per / 100 * len(arr))]
+    data = map(lambda x: map(lambda x: 0.0 if math.fabs(x) < threshold else x, x), data)
+    return data
+
+if __name__ == '__main__':
+    SIZE = 8 #ブロックサイズ
+
+    args = sys.argv #コマンドライン引数を取得
+    if len(args) != 3: #コマンドライン引数が3つでなければ	
+        print '引数が間違っています'
+    else:
+        img = cv2.imread(args[1], cv2.IMREAD_UNCHANGED) #読み込み
+        imf = np.float32(img) / 255.0
+        data = dct.imgSplit(imf, SIZE) #分割
+        data = dct.dct(data) #DCT変換
+
+        data = map(lambda x: map(lambda x: filter(x, float(args[2])), x), data) #フィルター処理
+        data = np.array(data)
+
+        data = dct.dct(data, flags = cv2.DCT_INVERSE) #DCT逆変換
+        img2 = dct.imgCombine(data) #結合
+        img2 = np.uint8(img2 * 255.0)
+
+        psnr = cv2.PSNR(img, img2) #SN比算出
+        print 'SN:',psnr,'dB'
+
+        cv2.imshow('image', img2)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
